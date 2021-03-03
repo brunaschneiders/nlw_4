602e5acceb954b12ee885172738690babbdad382
Bruna Letícia Schneiders <bruna.schneiders@gmail.com>
2021-03-03 08:49:53 -0300

    context api implementation

--- moveit-next/src/components/Countdown.tsx
+++ moveit-next/src/components/Countdown.tsx
@@ -1,51 +1,22 @@
 import {useState, useEffect, useContext} from 'react'
-import { ChallengesContext } from '../contexts/ChallengesContext';
+import { CountdownContext } from '../contexts/CountdownContext';
 
 import styles from "../styles/components/Countdown.module.css"
 
-let countdownTimeout: NodeJS.Timeout
-
 export function Countdown(){
-   const { startNewChallenge} = useContext(ChallengesContext)
-
-   const [time, setTime] = useState(0.05 * 60);
-   const [isActive, setIsActive] = useState(false);
-   const [hasFinished, setHasFinished] = useState(false);
-
-   // arredonda pra baixo
-   const minutes = Math.floor(time / 60);
-   // pega o resto da divisao por 60
-   const seconds = time % 60;
+   const { 
+      minutes, 
+      seconds, 
+      hasFinished, 
+      isActive, 
+      startCountdown, 
+      resetCountdown 
+   } = useContext(CountdownContext)
 
    // padStart está verificando se a string tem 2 caracteres. Se não tem, adiciona um 0
    // o array desetruturado pega os dois itens "gerados" no mesmo
    const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('')
    const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('')
-
-
-   function startCountdown(){
-      setIsActive(true)
-   }
-
-   function resetCountdown(){
-      // cancela a execução do setTimeout para que ele nao desconte mais um segundo depois do clique
-      clearTimeout(countdownTimeout)
-      setIsActive(false)
-      setTime(25 * 60)
-   }
-
-   useEffect(()=> {
-      if(isActive && time> 0){
-         countdownTimeout = setTimeout(()=>{
-            setTime(time - 1)
-         }, 1000)
-      } else if(isActive && time === 0){
-            setHasFinished(true)
-            setIsActive(false)
-            startNewChallenge()
-      }
-   }, [isActive, time])
-
     return (
        <div>
          <div className={styles.countdownContainer}>
--- moveit-next/src/pages/_app.tsx
+++ moveit-next/src/pages/_app.tsx
@@ -1,12 +1,16 @@
 import "../styles/global.css"
 
 import { ChallengesProvider} from "../contexts/ChallengesContext"
+import { CountdownProvider } from "../contexts/CountdownContext"
 
 function MyApp({ Component, pageProps }) {
   return (
     // todos os elementos dentro do provider terão acesso aos dados do contexto
+    // como o Countdown depende do Challenge, ele deve ficar por dentro do primeiro
     <ChallengesProvider>
-      <Component {...pageProps} />
+      <CountdownProvider>
+        <Component {...pageProps} />
+      </CountdownProvider>
     </ChallengesProvider>
     
   
